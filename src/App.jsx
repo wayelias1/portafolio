8ac94b8ec3797721bbfd3bfb5ee610b9f14bf7e5
wayelias1 <wayelias13@gmail.com>
2024-10-14 12:37:02 -0400

    acomdando diseño

--- src/App.jsx
+++ src/App.jsx
@@ -46,9 +46,9 @@ function App() {
   //   {scope: container}
   // );
 
-  let tl = gsap.timeline()
-       tl.to("#timeline-art-icon", { opacity: 1, duration: 0.5 })
-       tl.to("#timeline-art-box", { opacity: 1, x: 30, duration: 0.5 })
+  // let tl = gsap.timeline()
+  //      tl.to("#timeline-art-icon", { opacity: 1, duration: 0.5 })
+  //      tl.to("#timeline-art-box", { opacity: 1, x: 30, duration: 0.5 })
 
   const projectList = projects.map(({ titulo, imagen, link, descripcion, boton, skills, tipo = [] }) => {
 
@@ -112,8 +112,8 @@ function App() {
       <Navbar></Navbar>
 
       <section id='SobreMi' className='flex h-full justify-center'>
-        <div className='relative justify-center py-24 flex items-center flex-col h-1/2 lg:w-1/2 w-2/3'>
-          <img className='w-28 content-center rounded-full ' src="img/Elias.webp" alt="persona" />
+        <div className='relative justify-center py-24 flex items-center flex-col h-1/2 lg:w-1/2 md:w-2/3 w-full'>
+          <img className='w-28 content-center radiusRounded' src="img/Elias.webp" alt="persona" />
           <h1 className='dark:text-gray-100 text-black text-3xl'>Elías Cordova</h1>
           <p className='dark:text-blue-300 text-blue-900 brillo'>Front-end Developer</p>
           <div className='mt-5 rounded-lg shadow-lg dark:bg-gray-950 bg-gray-100 px-5 py-3 border border-gray-300 dark:border-gray-800 dark:shadow-gray-800'>
@@ -130,14 +130,14 @@ function App() {
           </div>
         </div>
       </section>
-      <section id='Proyectos' className='flex h-full justify-center flex-col items-center'>
-        <h3 className='text-blue-700 text-3xl dark:text-blue-200 brillo hover:'>Proyectos</h3>
-        <div className='py-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 xl:gap-10 max-w-screen-2xl jusitify-around mx-auto px-7' >
+      <section id='Proyectos' className='flex h-full justify-center flex-col items-center md:w-full'>
+        <h3 className='text-blue-900 text-3xl dark:text-blue-200 brillo hover:'>Proyectos</h3>
+        <div className='py-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 xl:gap-10 max-w-screen-2xl jusitify-around mx-auto px-4' >
           {projectList}
         </div>
       </section>
       <section id='Miexperiencia' className='flex h-screen justify-center py-6 my-4 flex-col items-center'>
-        <h4 className='text-blue-700 text-3xl dark:text-blue-200 brillo'>Mi experiencia</h4>
+        <h4 className='text-blue-900 text-3xl dark:text-blue-200 brillo'>Mi experiencia</h4>
         <div className='w-full flex flex-col justify-evenly items-center relative md:flex-row'>
           <div className='relative w-1/2'>
             <p className='text-black dark:text-white'>Mi primera experiencia trabajando como desarrollador web fue en la empresa
@@ -146,8 +146,8 @@ function App() {
           </div>
           <div id='timeline' className='relative py-4 before:absolute before:inset-0 before:h-full before:w-1 before:bg-gradient-to-b before:from-transparent dark:before:via-slate-300 before:via-black before:to-transparent before:mx-3'>
             <article id='timeline-art' className='is-active group relative flex w-full items-center justify-between md:w-2/4 xl:w-full xl:justify-normal xl:even:flex-row-reverse xl:even:text-right'>
-              <i id='timeline-art-icon' className='flex absolute h-5 w-5 opacity-0 items-center justify-center rounded-full bg-blue-500 mx-1'></i>
-              <div id='timeline-art-box' className='flex flex-col opacity-0 relative w-full h-full p-4 py-4 shadow-lg border min-w-max bg-indigo-100 border-gray-300 dark:bg-slate-900 dark:border-gray-800 dark:shadow-gray-800 dark:text-white rounded-lg backdrop-blur-lg transition hover:scale-105'>
+              <i id='timeline-art-icon' className='flex absolute h-5 w-5 items-center justify-center rounded-full bg-blue-500 mx-1'></i>
+              <div id='timeline-art-box' className='flex flex-col ml-8 relative w-full h-full p-4 py-4 shadow-lg border min-w-max bg-indigo-100 border-gray-300 dark:bg-slate-900 dark:border-gray-800 dark:shadow-gray-800 dark:text-white rounded-lg backdrop-blur-lg transition hover:scale-105'>
                 <h5>Movidagrafica</h5>
                 <p>FullStack Developer</p>
                 <span>2023</span>
@@ -157,7 +157,7 @@ function App() {
         </div>
       </section>
       <section id='Habilidades' className='flex justify-evenly py-6 my-4 flex-col items-center'>
-        <h4 className='text-blue-700 text-3xl dark:text-blue-200 brillo'>Mis Habilidades</h4>
+        <h4 className='text-blue-900 text-3xl dark:text-blue-200 brillo'>Mis Habilidades</h4>
         <div className='grid w-full gridskills mt-5 gap-2 md:gap-4 lg:gap-6 xl:gap-10 max-w-screen-2xl jusitify-around mx-auto px-5'>
 
           {mySkills}
